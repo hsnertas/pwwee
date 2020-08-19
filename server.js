964b65e3389160686fa0d0d8bac664dc25b78fce
hsnertas <hsnertas@gmail.com>
2020-08-19 04:42:48 +0300

    updates

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds031915.mlab.com:31915/heroku_cnl2pq42", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
